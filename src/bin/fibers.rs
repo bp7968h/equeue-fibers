d3d91d84b8f0bbf600bbf2f8fbac31ac7ff83c09
Bhuwan Pandit <bhuwanpandit109@gmail.com>
2024-12-22 19:23:34 +0000

    feat: add fibers simulation binary

---
+++ src/bin/fibers.rs
@@ -0,0 +1,28 @@
+use equeue_fibers::fibers::runtime::Runtime;
+use equeue_fibers::fibers::yield_thread;
+
+fn main() {
+    let mut runtime = Runtime::new();
+    runtime.init();
+
+    runtime.spawn(|| {
+        println!("THREAD 1 STARTING");
+        let id = 1;
+        for i in 0..10 {
+            println!("thread: {} counter: {}", id, i);
+            yield_thread();
+        }
+        println!("THREAD 1 FINISHED");
+    });
+
+    runtime.spawn(|| {
+        println!("THREAD 2 STARTING");
+        let id = 2;
+        for i in 0..15 {
+            println!("thread: {} counter: {}", id, i);
+            yield_thread();
+        }
+        println!("THREAD 2 FINISHED");
+    });
+    runtime.run();
+}
